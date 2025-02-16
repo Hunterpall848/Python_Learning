effd679f9d0efe39a2b2e41013d0f1c3d816be20
Hunter Pallister <hijrcm848@gmail.com>
2025-02-15 18:27:30 -0800

    Attribute Learning #2

--- Classes And OOP.py
+++ Classes And OOP.py
@@ -1,3 +1,6 @@
+import operator
+
+
 #First Class practice showing favorite things!
 class Favorites:
     def __init__(self, color, movie, song, animal):
@@ -23,4 +26,43 @@ for element in can_we_count_it:
   else:
     print(str(type(element)) + " does not have the count attribute :(")
 
+class Math:
+    def __init__ (self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        self.mult = num1 * num2
+        self.div = num1 / num2
+        self.sub = num1 / num2
+        self.add = num1 + num2
+
+
+
+numbers1 = Math(12, 34)
+print(numbers1.mult + numbers1.add - numbers1.div)
+
+
+class Student:
+
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+        self.grades = []
+
+
+    def add_grade(self, grade):
+        if type(grade) is Grade:
+            self.grades.append(grade)
+
+class Grade:
+    minimum_passing = 65
+
+    def __init__(self, score):
+        self.score = score
+
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+pieter.add_grade(Grade(100))
 
